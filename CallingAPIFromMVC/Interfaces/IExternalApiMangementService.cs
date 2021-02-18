d93eaedb9a866fceed4257c94ab26a17a293b11e
Hristijan Taseski <hristijan.taseski@gmail.com>
2021-02-18 11:28:30 +0100

    final commit

---
+++ CallingAPIFromMVC/Interfaces/IExternalApiMangementService.cs
@@ -0,0 +1,16 @@
+﻿using CallingAPIFromMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CallingAPIFromMVC.Interfaces
+{
+    public interface IExternalApiMangementService
+    {
+        List<Student> GetAll();
+        Student Create(Student student);
+        Student Update(Student student);
+        Student Delete(int ID);
+    }
+}
---
+++ CallingAPIFromMVC/Services/ExternalApiMangementService.cs
@@ -0,0 +1,37 @@
+﻿using CallingAPIFromMVC.Interfaces;
+using CallingAPIFromMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace CallingAPIFromMVC.Services
+{
+    public class ExternalApiMangementService : BaseApiService, IExternalApiMangementService
+    {
+        private const string BASE_API_URL = "baseurl here";
+        private const string BEARER_TOKEN = "baseurl here";
+       
+        public List<Student> GetAll()
+        {
+            
+            return SendRequest<string, List<Student>>($"{BASE_API_URL}", string.Empty, HttpMethod.Get, new AuthenticationHeaderValue("Basic", BEARER_TOKEN));
+           
+        }
+        public Student Create(Student student)
+        {
+            return SendRequest<Student, Student>($"{BASE_API_URL}/create", student, HttpMethod.Put, new AuthenticationHeaderValue("Bearer", BEARER_TOKEN));
+        }
+        public Student Update(Student student)
+        {
+            return SendRequest<Student, Student>($"{BASE_API_URL}/update", student, HttpMethod.Post, new AuthenticationHeaderValue("Bearer", BEARER_TOKEN));
+        }
+
+        public Student Delete(int ID)
+        {
+            return SendRequest<int, Student>($"{BASE_API_URL}/delete", ID, HttpMethod.Delete, new AuthenticationHeaderValue("Bearer", BEARER_TOKEN));
+        }
+    }
+}
